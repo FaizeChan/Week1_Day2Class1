7bd94e0f598e1fe8859ccba6e734774cf1ae9a4d
FaizeChan <obey.faize@gmail.com>
2017-10-03 17:05:28 -0400

    loops and switches

--- Week2_day2Class1/Program.cs
+++ Week2_day2Class1/Program.cs
@@ -10,6 +10,181 @@ namespace Week2_day2Class1
     {
         static void Main(string[] args)
         {
+            //for (int i = 0; i <= 10; i += 2)
+            //{
+            //    //   Console.WriteLine(i);
+            //}
+
+            //for (int counter = 50; counter <= 100; counter++)
+            //{
+            //    //Console.WriteLine(counter);
+            //}
+
+            ////string greetings = "My name is Faizon";
+            ////string[] words = greetings.Split();
+
+            ////for(int i = 0; i < words.Length;i++)
+            ////{
+            ////    Console.WriteLine(words[i]);
+
+            //REMEMBER cw tabtab is Console.WriteLine shortcut!!
+
+
+            //for(int i =1; is where you start.
+            //i <= 10; is where you're going to end
+            //i++ is adding the value by 1 
+
+
+
+            //for(int counter = 50; counter <= 100; counter++)
+            //{
+            //    Console.WriteLine(counter);
+            //}
+
+            //string greeting = "My name is Little Bill";
+            //string[] words = greeting.Split();
+
+            //for(int i = 0; i < words.Length; i++ )
+            //{
+            //    Console.Write(words[i] + " ");
+            //}
+
+            //string[] days = { "Monday", "Tuesday", "Wednesday" };
+
+            //for (int i = 0; i < days.Length; i++)
+            //{
+            //    Console.WriteLine(days[i]);
+            //}
+
+            //string storyWord = "Once upon a time";
+            //string[] words = storyWord.Split();
+
+            //for(int i = 0; i < words.Length; i++)
+            //{
+            //    Console.Write(words[i] + " ");
+            //}
+
+            //string[] months = { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
+            //foreach (string month in months)
+            //{
+            //    Console.WriteLine(month);
+            //}
+
+            //string[] musicians = { "Bruno Mars", "The Weeknd", "Drake", "Young Micheal Jackson", "Jamie Foxx" };
+
+            //foreach(string musician in musicians)
+            //{
+            //    Console.WriteLine(musician);
+            //}
+
+
+
+
+
+
+            //string[] favoreiteFoods = { "pizza", "steak", "lobster", "shrimp", "tortilla" };
+
+            //foreach(string foods in favoreiteFoods)
+            //{
+            //    Console.WriteLine(foods);
+            //}
+
+
+
+
+            //int[] luckyNumbers = { 1, 3, 5, 7, 9, 10, 15, 25, 47 };
+
+            //foreach(int luckyNumber in luckyNumbers)
+            //{
+            //    Console.WriteLine("Your lucky number is " + luckyNumber);
+            //}
+
+
+            //Console.WriteLine("Pick a number one through five.");
+            //Console.WriteLine("based on your number, I will tell you who the greatest superhero is" );
+            //Console.WriteLine("Enter your number now!");
+
+            //int userNumber = int.Parse(Console.ReadLine());
+            //string bestHero = " ";
+
+            //switch (userNumber)
+            //{
+            //    case 1:
+            //        bestHero = "Wonder Women";
+            //        break;
+            //    case 2:
+            //        bestHero = "Wolverine";
+            //        break;
+            //    case 3:
+            //        bestHero = "Spider-Man";
+            //        break;
+            //    case 4:
+            //        bestHero = "Batgirl";
+            //        break;
+            //    case 5:
+            //        bestHero = "Batman";
+            //        break;
+            //    default:
+            //        Console.WriteLine("No, I said 1-5!!");
+            //        Console.WriteLine("Please enter the correct number!!");
+            //        userNumber = int.Parse(Console.ReadLine());
+            //        break;
+
+            //}
+            //Console.WriteLine("The best super Hero is " + bestHero);
+
+            Console.WriteLine("What is your favorite CDMRW animal?");
+            Console.WriteLine("If you don't know what CDMRW stands for, type in (help)");
+
+            string favAnimal = Console.ReadLine().ToLower();
+
+            string snack;
+
+            if (favAnimal == "help")
+            {
+                Console.WriteLine("Cat\tDog\tMonkey\tReindeer\tWhale");
+                Console.WriteLine("Please enter your favorite CDMRW animal.");
+                favAnimal = Console.ReadLine().ToLower();
+            }
+
+            switch (favAnimal)
+            {
+
+                case "cat":
+                    snack = "tuna";
+                    break;
+                case "dog":
+                    snack = "bacon";
+                    break;
+                case "monkey":
+                    snack = "banana";
+                    break;
+                case "reindeer":
+                    snack = "carrot";
+                    break;
+                case "Whale":
+                    snack = "twinkies";
+                    break;
+                default:
+                    snack = "go hungry";
+                    break;
+
+            }
+
+
+            Console.WriteLine("Enjoy your healthy snack." + snack);
+
+
+
+
+
+
+
+
+
+
+
         }
     }
-}
+    }
+
